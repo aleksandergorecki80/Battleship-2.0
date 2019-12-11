2943a02dd23e4543607ebf032d330ce6bbd0b0f7
Aleksander Gorecki <aleksandergorecki80@gmail.com>
2019-12-11 07:33:39 +0000

    Add view object

--- js/app.js
+++ js/app.js
@@ -18,10 +18,31 @@ function printColumns(trId) {
     var td = document.createElement("td");
     tr.appendChild(td);
     
-    td.id = `td${i}`;
+    td.id = `${trId}td${i}`;
   }
 }
 
 for (let i = 0; i < 7; i++) {
   printRows(i);
 }
+
+
+const view = {
+    displayMessage: function(msg){
+        const messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location){
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function(location){
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+}
+
+// Testing
+view.displayMessage('This is a testing message');
+view.displayHit("tr3td3");
+view.displayMiss("tr6td4");
